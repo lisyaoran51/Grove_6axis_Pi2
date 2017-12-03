a8689f260a5643ab4570480701217794ae0c9551
TsaiJiaYu-PC\TsaiJiaYu <lisyaoran51@hotmail.com>
2017-12-04 02:51:05 +0800

    no message

--- main.c
+++ main.c
@@ -68,11 +68,16 @@ int main(int argc, char *argv[])
 
 	FILE *f = fopen("file.txt", "ab+");
 	if (f == NULL)
-{
-	f = fopen("file.txt", "wb");
-    //printf("Error opening file!\n");
-	//usleep(100000000000);
-}
+	{
+		f = fopen("file.txt", "wb");
+		if (f == NULL)
+		{
+			printf("Error opening file!\n");
+			usleep(100000000000);
+		}
+		//printf("Error opening file!\n");
+		//usleep(100000000000);
+	}
 
 
 	while(1)
