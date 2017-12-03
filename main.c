e11018d3d80b59de161f1668069b240ffcef11d9
TsaiJiaYu-PC\TsaiJiaYu <lisyaoran51@hotmail.com>
2017-12-03 23:35:33 +0800

    debug

--- main.c
+++ main.c
@@ -139,7 +139,7 @@ int main(int argc, char *argv[])
 	read (fd, readData, 2) ;
 	int16_t output0 = (int16_t)readData[0];
 	int16_t output1 = (int16_t)(readData[1] << 8);
-	int16_t output = (int16_t)readData[0] | int16_t(readData[1] << 8);
+	int16_t output = output0 | output1;
 	printf ("  %d\n", output) ;
 	//printf ("  %02X", readData [0]) ;
 	//printf ("  %02X\n", readData [1]) ;
